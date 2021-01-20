e1cb0667ae3a908b934599d22d845e0be1c130fc
cduffy31 <44772719+cduffy31@users.noreply.github.com>
2021-01-20 15:41:21 +0000

    Create fetchPrice.py
    this is a small class I created for downloading exchange rates with different options, it was attached to a front end to automize the processes

---
+++ fetchPrice.py
@@ -0,0 +1,33 @@
+from exchangeratesapi import Api
+
+
+class fetchPrice:
+    """
+    This class is using the central european banks API to get up to date prices of foreign exchange
+    Author:
+        Callan Duffy
+    param:
+        doesnt require anything but can use dates if required.
+        dates must be added as "yyyy-mm-dd"
+    return:
+        can either return a couple or a numpy array depending on what is asked.
+    """
+
+    def __init__(self):
+        # content becomes a dictionary
+        self.content = None
+        self.euro = 0
+        self.usd = 0
+        self.api = Api()
+
+    def get_price(self):
+        # opening the url and receiving the JSON object to the allow the class its self to use it
+        self.content = self.api.get_rates('GBP', ['USD', 'EUR'])
+        self.unpack()
+
+    def unpack(self):
+        self.euro = self.content['rates']['EUR']
+        self.usd = self.content['rates']['USD']
+
+    def get_data(self, start, end):
+        return self.api.get_rates('GBP', ['USD', 'EUR'], start, end)
